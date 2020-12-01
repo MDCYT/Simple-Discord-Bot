6a253e7a4791cca6a7242917e34c6a88248cd4bd
MDCYT <67084442+MDCYT@users.noreply.github.com>
2020-11-30 21:36:32 -0500

    Update ping.js

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,11 +1,15 @@
 
-module.exports.run = (client, message, args, utils) => {
+const {MessageEmbed} = require("discord.js")
+
+module.exports.run = async (client, message, args, utils) => {
+
+  const msg = await message.channel.send('`Pinging...`');
 
     const timestamp = (message.editedTimestamp) ? message.editedTimestamp : message.createdTimestamp; // Check if edited
     const latency = `\`\`\`ini\n[ ${Math.floor(msg.createdTimestamp - timestamp)}ms ]\`\`\``;
-    const apiLatency = `\`\`\`ini\n[ ${Math.round(message.client.ws.ping)}ms ]\`\`\``;
 
-    message.channel.send(`Ping: ${latency} n\Api: ${apiLatency}`)
+    msg.edit('Latency ' + latency);
+
 }
 
 
